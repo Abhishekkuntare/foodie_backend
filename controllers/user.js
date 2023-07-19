024cc9af06f9aa2d02fc1cb5212e5bc961a30cd4
Abhishek Kuntare <abhishekkuntare7@gmail.com>
2023-07-19 19:50:14 +0530

    logout

--- controllers/user.js
+++ controllers/user.js
@@ -13,7 +13,11 @@ export const logout = (req, res, next) => {
   req.session.destroy((err) => {
     if (err) return next(err);
 
-    res.clearCookie("connect.sid");
+    res.clearCookie("connect.sid", {
+      secure: process.env.NODE_ENV === "development" ? false : true,
+      httpOnly: process.env.NODE_ENV === "development" ? false : true,
+      sameSite: process.env.NODE_ENV === "development" ? false : "none",
+    });
     res.status(200).json({
       message: "Logged Out",
     });
@@ -30,7 +34,6 @@ export const getAllUsers = asyncError(async (req, res, next) => {
 });
 
 export const getAdminStats = asyncError(async (req, res, next) => {
-  
   const userCount = await User.countDocuments();
 
   const orders = await Order.find({});
