d7b83174c0db85cf7c71874e8267f8cf1876622b
Abhishek Kuntare <abhishekkuntare7@gmail.com>
2023-07-25 18:28:57 +0530

    done

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,17 +1,27 @@
 import ErrorHandler from "../utils/ErrorHandler.js";
+import { asyncError } from "./errorMiddleware.js";
+import jwt from "jsonwebtoken";
+import { LoginRegisterUser } from "../models/LoginRegister.js";
 
-export const isAuthenticated = (req, res, next) => {
-  const token = req.cookies["connect.sid"];
+export const isAuthenticated = asyncError(async (req, res, next) => {
+  const { token } = req.cookies;
 
-  if (!token) {
-    return next(new ErrorHandler("Not logged In", 401));
-  }
+  if (!token) return next(new ErrorHandler("Not logged In !", 401));
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+  req.user = await LoginRegisterUser.findById(decoded._id);
+
   next();
-};
+});
 
 export const authorizedAdmin = (req, res, next) => {
-  if (req.user.role !== "admin") {
-    return next(new ErrorHandler("Only admin can Access", 405));
-  }
+  if (req.user.role !== "admin")
+    return next(
+      new ErrorHandler(
+        `${req.user.role} is not allowed to access the resource`,
+        403
+      )
+    );
   next();
 };
